c87a2aa040c0b2e510a87e01d2519f81e4df6920
omooba1 <iyoboyidavid@gmail.com>
2021-08-04 14:26:10 +0100

    Retrofit

--- app/src/main/java/com/iyoboyi/retrofitapp/API/PlaceHolderAPI.kt
+++ app/src/main/java/com/iyoboyi/retrofitapp/API/PlaceHolderAPI.kt
@@ -7,5 +7,5 @@ import retrofit2.http.GET
 interface PlaceHolderAPI {
 
     @GET("users")
-    fun getUsers(): Call<List<User>>
+    suspend fun getUsers(): List<User>
 }
--- app/src/main/java/com/iyoboyi/retrofitapp/UI/MainViewModel.kt
+++ app/src/main/java/com/iyoboyi/retrofitapp/UI/MainViewModel.kt
@@ -4,28 +4,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.iyoboyi.retrofitapp.API.RetrofitProvider
 import com.iyoboyi.retrofitapp.model.User
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     val usersLiveData = MutableLiveData<List<User>>()
 
     fun getUsers() {
-        RetrofitProvider.placeHolderAPI.getUsers().enqueue(object : Callback<List<User>> {
-            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                response.body()?.let {
-                    usersLiveData.postValue(it)
-                }
-            }
-
-            override fun onFailure(call: Call<List<User>>, t: Throwable) {
-
-            }
-
-
-        })
+        CoroutineScope(Dispatchers.IO).launch {
+            usersLiveData.postValue(RetrofitProvider.placeHolderAPI.getUsers())
+        }
 
     }
 }
